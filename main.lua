e789ee5fdbb7d813963304fdce623f8fbf955d44
Jose Carlos Esmeraldo <zeca.esmeraldo@gmail.com>
2018-09-19 16:28:03 -0300

    Changing max's running sprite, fireballs rotation, adding walls to stop blue in the end and start of map

--- main.lua
+++ main.lua
@@ -3,20 +3,19 @@ physics.start()
 physics.setGravity( 0, 0 )
 --physics.setDrawMode("hybrid")
 
+
+leftWall = display.newRect(0, display.contentHeight/2, 1, display.contentHeight )
+physics.addBody(leftWall, "static", ({density=3.0, friction=0.5, bounce=0}))
+leftWall.myName = "leftWall"
+rightWall = display.newRect(display.contentWidth, display.contentHeight/2, 1, display.contentHeight )
+physics.addBody(rightWall, "static", ({density=3.0, friction=0.5, bounce=0}))
+rightWall.myName = "leftWall"
+
+local ground = display.newRect(display.contentWidth/2, 640, display.contentWidth, 90 )
+physics.addBody(ground, "static", {density=3.0, friction=0.5, bounce=0})
+ground.myName = "ground"
+
 local background = display.newImageRect( "assets/background.png", 1280, 720 )
-
-local dinoOptions =
-{
-	--required parameters
-	width = 24,
-	height = 50,
-	numFrames = 24,
-
-	--optional parameters; used for scaled content support
-	sheetContentWidth = 576,  -- width of original 1x size of entire sheet
-	sheetContentHeight = 50   -- height of original 1x size of entire sheet
-}
-local dinoImageSheet = graphics.newImageSheet( "assets/sprites/blue.png", dinoOptions )
 
 
 background.anchorX = 0
@@ -31,77 +30,76 @@ end
 
 reset_landscape( background )
 
--- consecutive frames
+local dinoOptions =
+{
+	width = 24,
+	height = 50,
+	numFrames = 24,
+	sheetContentWidth = 576,
+	sheetContentHeight = 50
+}
+local dinoImageSheet = graphics.newImageSheet( "assets/sprites/blue.png", dinoOptions )
 local dinoSequenceData =
 {
+	{
+		name="walking",
+		time = 800,
+		loopCount = 0,        -- Optional ; default is 0
+		frames = { 4,5,6,7,8,9,10 }
+		--	loopDirection = "bounce"
+	},
+	{
 	name="running",
 	time = 800,
-	loopCount = 0,        -- Optional ; default is 0
-	frames = { 4,5,6,7,8,9,10 }
---	loopDirection = "bounce"
+	loopCount = 0,
+	frames = { 18,19,20,21,22,23 }
+	}
 }
 
-
 local blue = display.newSprite( dinoImageSheet, dinoSequenceData )
-
-
-
-
+blue:scale(3, 1.5)
+blue.x = 50
+blue.y = 558
+physics.addBody( blue, "dynamic", { radius=30, isSensor=true } )
+blue.myName = "blue"
+blue:play()
 
 local fireBallOptions =
 {
-	--required parameters
 	width = 48,
 	height = 48,
 	numFrames = 6,
-
-	--optional parameters; used for scaled content support
-	sheetContentWidth = 288,  -- width of original 1x size of entire sheet
-	sheetContentHeight = 48 -- height of original 1x size of entire sheet
+	sheetContentWidth = 288,
+	sheetContentHeight = 48
 }
 local fireBallImageSheet = graphics.newImageSheet( "assets/sprites/fireball.png", fireBallOptions )
-
--- consecutive frames
 local fireBallSequenceData =
 {
 	name="falling",
 	time = 800,
-	loopCount = 0,        -- Optional ; default is 0
+	loopCount = 0,
 	start = 1,
 	count = 6
 }
-
-
-
-
-
 local fireBallTable = {}
 local function createFireball()
 	local fireball = display.newSprite( fireBallImageSheet, fireBallSequenceData )
 	fireball:scale(-1, -1)
 	fireball:scale(4, 4)
-	fireball.rotation = -85
---	fireball.x = 600
---	fireball.y = 280
+	fireball.rotation = -50
 	fireball:play()
---	local newAsteroid = display.newImageRect( mainGroup, objectSheet, 1, 102, 85 )
 	table.insert( fireBallTable, fireball )
-	physics.addBody( fireball, "dynamic", { radius=40, bounce=0.8 } )
+	physics.addBody( fireball, "kinematic", { radius=40, bounce=0.8 } )
 	fireball.myName = "fireball"
 	-- From the top
-	fireball.x = math.random( display.contentWidth )
+	fireball.x = math.random( display.contentWidth, (display.contentWidth + 80) )
 	fireball.y = -60
-	fireball:setLinearVelocity( math.random( -40,40 ), math.random( 40,120 ) )
-
---	fireball:applyTorque( math.random( -6,6 ) )
+	fireball:setLinearVelocity( math.random( -140,-10 ), math.random( 40,120 ) )
 end
 
 local function gameLoop()
-
-	-- Create new asteroid
 	createFireball()
-
-	-- Remove asteroids which have drifted off screen
+	-- Remove fireball which have drifted off screen
 	for i = #fireBallTable, 1, -1 do
 		local thisFireBall = fireBallTable[i]
 
@@ -115,33 +113,24 @@ local function gameLoop()
 		end
 	end
 end
-
-gameLoopTimer = timer.performWithDelay( 500, gameLoop, 0 )
-
+createFireball()
 
 
 
-blue:scale(3, 1.5)
-blue.x = 50
-blue.y = 558
-physics.addBody( blue, { radius=30, isSensor=true } )
-blue.myName = "blue"
-blue:play()
-
--- Touch event listener
+gameLoopTimer = timer.performWithDelay( 900, gameLoop, 0 )
 local function touchListener( event )
-	print( "Phase: " .. event.phase )
---	print( "Location: " .. tostring(event.x) .. "," .. tostring(event.y) )
---	print( "Unique touch ID: " .. tostring(event.id) )
---	print( "----------" )
 	if(event.phase == "began" or event.phase == "moved") then
-		blue:setLinearVelocity( 50, 0 )
+		blue:setSequence("running")
+		blue:play()
+		blue:setLinearVelocity( 80, 0 )
 	else
-		blue:setLinearVelocity( -50, 0 )
+		blue:setSequence("walking")
+		blue:play()
+		blue:setLinearVelocity( -80, 0 )
 	end
 	return true
 end
-background:addEventListener( "touch", touchListener )
+Runtime:addEventListener( "touch", touchListener )
 
 local function onCollision( event )
 
@@ -154,20 +143,17 @@ local function onCollision( event )
 				( obj1.myName == "fireball" and obj2.myName == "blue" ) )
 		then
 			print("Collision")
---			if ( died == false ) then
---				died = true
---
---				-- Update lives
---				lives = lives - 1
---				livesText.text = "Lives: " .. lives
---
---				if ( lives == 0 ) then
---					display.remove( blue )
---				else
---					blue.alpha = 0
--- --					timer.performWithDelay( 1000, restoreShip )
---				end
---			end
+
+		elseif ((obj1.myName == "leftWall" and obj2.myName == "blue") or
+				(obj1.myName == "blue" and obj2myName == "leftWall") or
+				(obj1.myName == "blue" and obj2myName == "rightWall") or
+				(obj1.myName == "rightWall" and obj2myName == "blue") )
+		then
+			blue:setSequence("walking")
+			blue:play()
+			blue:setLinearVelocity( 0, 0 )
+		else
+
 		end
 	end
 end
